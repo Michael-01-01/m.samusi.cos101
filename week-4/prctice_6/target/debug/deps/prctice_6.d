c2a28f9ff9911d98bea5a16e06820235a2dfb44f
Michael-01-01 <michael.samusi@pau.edu.ng>
2025-10-22 19:16:50 +0100

    (My sixth commit for week 2)

---
+++ week-4/prctice_6/target/debug/deps/prctice_6.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Documents\m.samusi.cos101\week-4\prctice_6\target\debug\deps\prctice_6.d: src\main.rs
+
+C:\Users\Admin\Documents\m.samusi.cos101\week-4\prctice_6\target\debug\deps\prctice_6.exe: src\main.rs
+
+src\main.rs:
