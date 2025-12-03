cffa784117ee0a3a3833a5a2be2d6ce518afc032
Michael-01-01 <michael.samusi@pau.edu.ng>
2025-12-03 16:55:16 +0100

    (Ownership and Structures)

---
+++ week-10/practice_8/src/main.rs
@@ -0,0 +1,25 @@
+struct Employee {
+    ceo:String,
+    company:String,
+    age:u8,
+}
+
+fn main() {
+    let emp1 = Employee {
+        company:String::from("Microsoft corporation"),
+        ceo:String::from("Satya Nadella"),
+        age:56
+    };
+
+    let emp2 = Employee {
+        company:String::from("Google Inc"),
+        ceo:String::from("Sundai Pichai"),
+        age:51
+    };
+    display(emp1);
+    display(emp2);
+    }
+
+fn display(emp:Employee){
+    println!("Name is :{} company is {} age is {}",emp.ceo,emp.company,emp.age);
+}
